743ffeb59b8c81595abe9fa724f07e73f666bbfa
hao jia <>
2018-10-14 22:20:42 +0800

    netty

---
+++ jaf-tcp-server/src/main/java/com/jaf.tcp.server/DiscardServerHandler.java
@@ -0,0 +1,30 @@
+package com.jaf.tcp.server;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.buffer.ByteBuf;
+import io.netty.util.ReferenceCountUtil;
+
+public class DiscardServerHandler extends ChannelHandlerAdapter {
+
+//    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        // Discard the received data silently.
+//        ((ByteBuf) msg).release(); // (3)
+        try {
+            // Do something with msg
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
